960a3225561999ec25898fcb38e02b87b015d76c
Weikai Kong <priv@pppig236.com>
2025-11-02 23:55:48 -0500

    sm8750-common: Update from OOS 16
    Change-Id: I9da88c3e2e8f13f99850e16c96c139de4024aed3
    Signed-off-by: Weikai Kong <priv@pppig236.com>

--- extract-files.py
+++ extract-files.py
@@ -35,13 +35,8 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
-        'com.qualcomm.qti.dpm.api@1.0',
         'vendor.qti.ImsRtpService-V1-ndk',
         'vendor.qti.diaghal-V1-ndk',
-        'vendor.qti.hardware.dpmaidlservice-V1-ndk',
-        'vendor.qti.hardware.wifidisplaysession_aidl-V1-ndk',
-        'vendor.qti.qccsyshal_aidl-V1-ndk',
-        'vendor.qti.qccvndhal_aidl-V1-ndk',
     ): lib_fixup_vendor_suffix,
     (
         'libagm',
@@ -76,7 +71,10 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.audio.core.sounddose-V1-ndk.so', 'android.hardware.audio.core.sounddose-V2-ndk.so')
         .replace_needed('android.hardware.audio.common-V1-ndk.so', 'android.hardware.audio.common-V3-ndk.so')
         .replace_needed('libaudio_aidl_conversion_common_ndk.so', 'libaudio_aidl_conversion_common_ndk_prebuilt.so'),
-    'vendor/lib64/libaudioserviceexampleimpl.so': blob_fixup()
+    (
+        'vendor/lib64/hw/android.hardware.bluetooth.audio_sw.so',
+        'vendor/lib64/libaudioserviceexampleimpl.so',
+    ): blob_fixup()
         .replace_needed('android.media.audio.common.types-V4-ndk.so', 'android.media.audio.common.types-V3-ndk.so')
         .replace_needed('android.hardware.bluetooth.audio-impl.so', 'android.hardware.bluetooth.audio-impl_prebuilt.so')
         .replace_needed('libbluetooth_audio_session_aidl.so', 'libbluetooth_audio_session_aidl_prebuilt.so')
@@ -105,6 +103,11 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libsdmclient.so',
     ): blob_fixup()
         .replace_needed('vendor.qti.hardware.display.config-V11-ndk.so', 'vendor.qti.hardware.display.config-V12-ndk.so'),
+    (
+        'vendor/lib64/libloc_core.so',
+        'vendor/lib64/libloc_api_v02.so',
+    ): blob_fixup()
+        .add_needed('libbase.so'),
     'vendor/lib64/libqcodec2_core.so': blob_fixup()
         .add_needed('libcodec2_shim.so'),
     'vendor/lib64/libwfdmmsrc_proprietary.so': blob_fixup()
