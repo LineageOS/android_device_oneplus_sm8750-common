54c4dc76f227ffba00b8157c904f8cedb1cb24ba
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-24 03:08:29 +0800

    sm8750-common: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57

--- common.mk
+++ common.mk
@@ -268,7 +268,7 @@ $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.oplus
 
-$(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
+$(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
 # Media
 PRODUCT_COPY_FILES += \
