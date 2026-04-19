4942bbfeedc040db5f46aa5f06f07dba6b6713fd
chandu078 <chandudyavanapelli03@gmail.com>
2026-04-19 08:56:47 +0000

    sm8750-common: Use foreach for OMAPI UUID map copies
    Change-Id: I86a96aae4c024ab478069de4a3bdeeae9e1e7f8f

--- common.mk
+++ common.mk
@@ -364,11 +364,14 @@ ifneq ($(TARGET_IS_TABLET),true)
 PRODUCT_PACKAGES += \
     SecureElementResTarget_Vendor
 
+OMAPI_UUID_MAP_IDS := 23 29 31
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/omapi/hal_uuid_map_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hal_uuid_map_23.xml \
-    $(LOCAL_PATH)/configs/omapi/hal_uuid_map_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hal_uuid_map_29.xml \
-    $(LOCAL_PATH)/configs/omapi/hal_uuid_map_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hal_uuid_map_31.xml \
-    $(LOCAL_PATH)/configs/omapi/hal_uuid_map_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hal_uuid_map_config.xml \
+    $(foreach UUID_MAP_ID, $(OMAPI_UUID_MAP_IDS), \
+        $(LOCAL_PATH)/configs/omapi/hal_uuid_map_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hal_uuid_map_$(UUID_MAP_ID).xml) \
+    $(LOCAL_PATH)/configs/omapi/hal_uuid_map_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hal_uuid_map_config.xml
+
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/com.android.se.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.se.xml
 endif
 
